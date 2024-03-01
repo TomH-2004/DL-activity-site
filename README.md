d38a53db735317538d03a3cab909d7331ee42b1a
TomH-2004 <tom@datalords.net>
2024-03-01 15:53:45 +0000

    nav bar, more pages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DL-activity-site
+CREATE TABLE user (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user (username, password) VALUES ('tom', '123');
--- cmd/main.go
+++ cmd/main.go
@@ -28,17 +28,41 @@ func main() {
 	defer db.Close()
 
 	http.HandleFunc("/", loginPage)
-	http.HandleFunc("/dashboard", authMiddleware(dashboardPage))
+	http.HandleFunc("/home", authMiddleware(homePage))
+	http.HandleFunc("/challenges", authMiddleware(challengesPage))
+	http.HandleFunc("/page3", authMiddleware(page3Page))
+	http.HandleFunc("/page4", authMiddleware(page4Page))
+	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 }
 
+// HANDLER FUNCTIONS
 func loginPage(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "login.html", nil)
 }
 
-func dashboardPage(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "dashboard.html", nil)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "home.html", nil)
+}
+
+func challengesPage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "challenges.html", nil)
+}
+
+func page3Page(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "page3.html", nil)
+}
+
+func page4Page(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "page4.html", nil)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +85,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	session.Values["authenticated"] = true
 	session.Save(r, w)
 
-	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
 }
 
 func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
